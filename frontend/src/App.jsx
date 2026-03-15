41386efabe419ef5ef6f905494136861b9a8fb06
priyarajak <priyarajakprl@gmail.com>
2026-03-15 15:29:11 +0530

    fix router animation setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,87 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Inquiry from "./pages/Inquiry";
+import MobileNav from "./components/MobileNav";
+
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import MobileNav from "./components/MobileNav";
 import Login from "./pages/Login";
 import InquiryCart from "./pages/InquiryCart";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { AnimatePresence } from "framer-motion";
-function App() {
+
+
+function AnimatedRoutes() {
+
   const location = useLocation();
+
   return (
-    <div className="flex flex-col min-h-screen">
-
-      <BrowserRouter>
-
-  <Navbar />
-
-  <main className="flex-grow">
 
     <AnimatePresence mode="wait">
 
       <Routes location={location} key={location.pathname}>
 
         <Route path="/" element={<Home />} />
+
         <Route path="/products" element={<Products />} />
+
         <Route path="/inquiry-cart" element={<InquiryCart />} />
+
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
 
       </Routes>
 
     </AnimatePresence>
 
-  </main>
-
-  <MobileNav />
-  <Footer />
-
-</BrowserRouter>
-
-    </div>
   );
+
+}
+
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <div className="flex flex-col min-h-screen">
+
+        <Navbar />
+
+        <main className="flex-grow">
+
+          <AnimatedRoutes />
+
+        </main>
+
+        <MobileNav />
+
+        <Footer />
+
+        <ToastContainer position="top-right" autoClose={2000} />
+
+      </div>
+
+    </BrowserRouter>
+
+  );
+
 }
 
 export default App;
